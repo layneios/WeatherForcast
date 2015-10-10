8f5bca7ae652c16d21336e4f222769c2f58fca39
LorinRain <tao4256@163.com>
2015-10-10 10:11:56 +0800

    Initial Commit

---
+++ WeatherForcast/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherForcast
+//
+//  Created by Lorin on 15/10/10.
+//  Copyright © 2015年 Lighting-Vista. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeatherForcast/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WeatherForcast
+//
+//  Created by Lorin on 15/10/10.
+//  Copyright © 2015年 Lighting-Vista. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
